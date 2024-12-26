f3d33419c3e412135da8ca4cf2363f1b66080a34
genius-ayush <ayushsanjayrawal@gmail.com>
2024-12-26 23:46:27 +0530

    25dec

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,22 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 function Navbar() {
   return (
     <>
-      <nav className="fixed w-full z-50 backdrop-blur-lg   border-b-gray-700 border-b-[0.1px]">
-        <div className="flex justify-center h-[72px] w-2/5 items-center lg:px-20 px-3 shadow-sm">
-          <div className='flex items-center font-semibold gap-2'>
-            <img src="/icon.png" alt="" className='w-9 h-9 	'/>
-            <span className='leading-10 text-xl'>Alex</span>
+      <nav className="fixed w-full z-50 backdrop-blur-lg  ">
+        <div className="flex justify-between items-center h-[72px] w-full lg:w-3/4 mx-auto px-6 lg:px-20 shadow-sm">
+          {/* Logo Section */}
+          <div className="flex items-center font-semibold gap-2">
+            <img src="/icon.png" alt="Logo" className="w-9 h-9" />
+            <span className="leading-10 text-xl">Alex</span>
           </div>
 
-          <div>
-            
+          {/* Navigation Links */}
+          <div className="flex gap-6 text-gray-400 ">
+            <div className='hover:text-white hover:pointer'>Home</div>
+            <div className='hover:text-white'>Feature</div>
+            <div className='hover:text-white'>Docs</div>
+            <div className='hover:text-white'>Pricing</div>
           </div>
         </div>
       </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
